59d565d581cf546e83ff40fcb7aca3b96e4fd204
Hans Reichgelt <hreichgelt06@gmail.com>
2023-01-26 21:10:07 -0600

    attempting render deploy

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+const routes = require('../controllers');
 
-const sequelize = require('./config/connection');
+const sequelize = require('../config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
